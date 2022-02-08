6b5285014da6c947e19fcb6cff8ce61f5b4ceeaa
Lars <l.gessner@posteo.de>
2022-02-08 10:55:36 +0100

    addsome basic styling and api call

--- src/App.js
+++ src/App.js
@@ -1,12 +1,43 @@
 import './App.css';
+import {useState, useEffect} from 'react';
+import key from "./api"
 
 function App() {
+  const [data, setData] = useState({
+    fact: "loading...",
+    source: "loading..."  
+  })
+
+  const url = "https://rest.blackhistoryapi.io/fact/random"
+
+  useEffect(() => {
+    fetch(url, {
+      headers: {
+        "accept": "application/json",
+        'x-api-key': key
+      }
+    }).then(response => response.json())
+      .then(randomFact => setData(prev => ({
+        fact: randomFact.Results[0].text,
+        source: randomFact.Results[0].source
+      })))
+      .catch(Error => console.log(Error))
+  }, [])
+    
+
+
   return (
     <div className="App">
-      <h1>Title</h1>
+      <header className='header'>
+        <h1 className='header--title'>it.matters</h1>  
+      </header>
+      <h2>informations</h2>
       <div className='main--wrapper'>
-
+        <h3>Get some facts</h3>
+        <p>{data.fact}</p>
+        <a href={data.source}>source</a>
       </div>
+      <button className='getData-btn'>GET.fact</button>
     </div>
   );
 }
